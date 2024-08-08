115c49a9b0a0386400452c9f546d4dd432aa1d24
purushothaman-003 <003purushoth@gmail.com>
2024-08-08 05:45:33 -0700

    update project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Command } from 'cmdk'
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const CommandMenu = () => {
+  return(
+    <Command className='command'>
+
+      <Command.Input className='input' placeholder='search here...' />
+
+        <Command.List>
+
+          <Command.Empty>No results found</Command.Empty>
+
+        <Command.Group>
+
+          <Command.Item className='item'>BMW</Command.Item>
+          <Command.Item className='item'>Benz</Command.Item>
+          <Command.Item className='item'>Audi</Command.Item>
+          <Command.Item className='item'>Tata</Command.Item>
+          <Command.Item className='item'>Jaguar</Command.Item>
+          <Command.Item className='item'>KIA</Command.Item>
+          <Command.Item className='item'>Swift</Command.Item>
+
+        </Command.Group>
+
+        </Command.List>
+
+    </Command>
+  )
 }
 
-export default App;
+export default CommandMenu
